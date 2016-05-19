abbf801602a7aa5e5c09f924ef679ba9e23d0718
Lee Dong-ju <ehdwn632@naver.com>
2016-05-19 11:25:53 +0900

    androidStudio First

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.user.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +15,23 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-2475-9007"));
+                        startActivity(intent);
+                    }
+                }
+        );
+        findViewById(R.id.button2).setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+
+                    }
+                }
+        );
+
+
     }
 
     @Override
