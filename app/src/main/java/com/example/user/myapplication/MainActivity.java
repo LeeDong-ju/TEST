a3f203622428e7a0d0164e5edf3b190b57556afd
Lee Dong-ju <ehdwn632@naver.com>
2016-05-24 14:12:33 +0900

    16.05.24 각주//aa

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.user.myapplication;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
+    List<ScanResult> scanResult;
+    WifiManager wm;
+    public void searchWifi() {
+        scanResult= wm.getScanResults();
+        Toast.makeText(getApplication() ,"wifi scan\nindex 0: "+ scanResult.get(0).SSID+"\nsize: "+scanResult.size(),Toast.LENGTH_LONG).show();
+    }
+    private BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                searchWifi();
+            }
+        }
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,17 +45,21 @@ public class MainActivity extends ActionBarActivity {
                         Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-2475-9007"));
                         startActivity(intent);
                     }
-                }//류건환
-        );
+                }
+        );//aa
         findViewById(R.id.button2).setOnClickListener(
                 new Button.OnClickListener(){
+                    //Wifi Scan -
                     public void onClick(View v){
+                        wm = (WifiManager) getSystemService(WIFI_SERVICE);
+                        wm.startScan();
+                        IntentFilter filter = new IntentFilter();
+                        filter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+                        registerReceiver(wifiReceiver, filter);
 
                     }
                 }
         );
-
-
     }
 
     @Override
